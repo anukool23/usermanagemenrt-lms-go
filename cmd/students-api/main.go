9ea27797d93946387144854719bba2ce75672ce7
Anukool Patel <151703654+anukool23@users.noreply.github.com>
2026-03-31 21:17:26 +0530

    Added channel to handle intrrupts

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/anukool23/usermanagement-lms-go/internal/config"
+)
 
 func main() {
-	fmt.Println("Welcome to Students API")
+	//load config
+	cfg := config.MustLoad()
+
+	//database setup
+
+	//setup router
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to students API"))
+	})
+	//setup server
+	server := http.Server{
+		Addr:    cfg.Port,
+		Handler: router,
+	}
+
+	log.Printf("Server started on %s", cfg.Port)
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("Failed to start server: ", err)
+		}
+	}()
+	<-done
 }
